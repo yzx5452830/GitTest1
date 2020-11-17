dcfc3466560309d20eb39a9ffcca44fb92114d8b
yuzixiao <yuzixiao@gg.com>
2020-11-17 14:39:37 +0800

    修改脚本
    修改脚本03

---
+++ GitTestProject/Assets/Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("git测试脚本");
+        
+        print("git测试脚本 修改02");
+		
+		print("git测试脚本 修改03");
+
+    }
+
+   
+}
